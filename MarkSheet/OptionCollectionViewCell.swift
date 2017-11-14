c7b02742d6a7d07584b5e2b347e739129165fddf
xenop <nejisiki2047@gmail.com>
2017-11-15 01:32:35 +0900

    change color

--- MarkSheet/OptionCollectionViewCell.swift
+++ MarkSheet/OptionCollectionViewCell.swift
@@ -22,15 +22,23 @@ class OptionCollectionViewCell: UICollectionViewCell {
             switch(markState) {
             case .noMark:
                 label?.backgroundColor = UIColor.clear
+                label?.textColor = UIColor.black
+                label?.layer.borderWidth = 1
                 isSelected = false
             case .marked:
-                label?.backgroundColor = UIColor.lightGray
+                label?.backgroundColor = UIColor(red: 21/255.0, green: 111/255.0, blue: 223/255.0, alpha: 1)
+                label?.textColor = UIColor.white
+                label?.layer.borderWidth = 0
                 isSelected = true
             case .correct:
-                label?.backgroundColor = UIColor.green
+                label?.backgroundColor = UIColor(red: 76/255.0, green: 216/255.0, blue: 101/255.0, alpha: 1)
+                label?.textColor = UIColor.white
+                label?.layer.borderWidth = 0
                 isSelected = true
             case .wrong:
-                label?.backgroundColor = UIColor.red
+                label?.backgroundColor = UIColor(red: 0.92, green: 0.3, blue: 0.24, alpha: 1)
+                label?.textColor = UIColor.white
+                label?.layer.borderWidth = 0
             }
         }
     }

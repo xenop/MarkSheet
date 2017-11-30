d238b4c2d77ee20643071c326a6372c1005db6a6
xenop <nejisiki2047@gmail.com>
2017-11-30 14:03:27 +0900

    add debug menu to toggle language

--- MarkSheet/DebugViewController.swift
+++ MarkSheet/DebugViewController.swift
@@ -15,7 +15,17 @@ class DebugViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        FormatViewController.showIntro()
+        if indexPath.row == 0 {
+            FormatViewController.showIntro()
+        } else if indexPath.row == 1 {
+            var lang = UserDefaults.standard.string(forKey: "i18n_language")
+            if lang == nil || lang == "ja" {
+                lang = "en"
+            } else {
+                lang = "ja"
+            }
+            UserDefaults.standard.set(lang, forKey: "i18n_language")
+        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }

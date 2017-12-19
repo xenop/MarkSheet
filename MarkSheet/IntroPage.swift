5c88ee82576926cfd23e28d2e9dbc80770252b95
xenop <nejisiki2047@gmail.com>
2017-12-19 17:03:57 +0900

    add buttons

--- MarkSheet/IntroPage.swift
+++ MarkSheet/IntroPage.swift
@@ -11,10 +11,22 @@ import UIKit
 class IntroPage: UIView {
     @IBOutlet var titleLabel:UILabel?
     @IBOutlet var descriptionLabel:UILabel?
+    @IBOutlet var playButton:UIButton?
+    @IBOutlet var closeButton:UIButton?
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        titleLabel!.text = NSLocalizedString("intro_page_title_1", comment: "")
-        descriptionLabel!.text = NSLocalizedString("intro_page_description_1", comment: "")
+        titleLabel!.text = NSLocalizedString("intro_page_title", comment: "")
+        descriptionLabel!.text = NSLocalizedString("intro_page_description", comment: "")
+        playButton!.setTitle(NSLocalizedString("intro_page_play_button", comment: ""), for: .normal)
+        closeButton!.setTitle(NSLocalizedString("intro_page_close_button", comment: ""), for: .normal)
+    }
+    
+    @IBAction func didPushPlayButton(sender: Any) {
+        print("play")
+    }
+    
+    @IBAction func didPushCloseButton(sender: Any) {
+        print("close")
     }
 }

c26051ec142d29fc957d7327441baebbfd18167f
xenop <nejisiki2047@gmail.com>
2017-12-20 15:30:06 +0900

    order by use

--- MarkSheet/FormatViewController.swift
+++ MarkSheet/FormatViewController.swift
@@ -13,11 +13,15 @@ import AMPopTip
 import AVKit
 
 class FormatViewController: UITableViewController, IntroViewDelegate {
+    // MARK: Properties
+    
     var managedObjectContext:NSManagedObjectContext?
     var formats:[Format]? = nil
     var selectedFormat:Format? = nil
     var isEditMode:Bool = false
     var introView:IntroView? = nil
+    
+    // MARK: - View Controller
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +39,117 @@ class FormatViewController: UITableViewController, IntroViewDelegate {
             UserDefaults.standard.set(true, forKey: .introDidShow)
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DisplayAnswerSheetListView" {
+            let destinationVC = segue.destination as! AnswerSheetListViewController
+            destinationVC.format = selectedFormat
+        } else if segue.identifier == "DisplayMakeFormatView" {
+            let nc = segue.destination as! UINavigationController
+            let destinationVC = nc.topViewController as! MakeFormatViewController
+            if isEditMode {
+                destinationVC.editFormat = selectedFormat
+                isEditMode = false
+            }
+            destinationVC.managedObjectContext = managedObjectContext!
+            destinationVC.completionHandler = {
+                self.loadData()
+            }
+        }
+    }
+    
+    // MARK: - TableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return formats?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let format = formats![indexPath.row]
+        cell.textLabel?.text = format.name
+        return cell
+    }
+    
+    // MARK: - TableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.setSelected(false, animated: false)
+        
+        selectedFormat = formats![indexPath.row]
+        self.performSegue(withIdentifier: "DisplayAnswerSheetListView", sender: self)
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .normal, title: "delete".localized) { (rowAction, indexPath) in
+            let alert = UIAlertController(title:"", message: "confirm delete".localized,
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            let delete = UIAlertAction(title: "delete".localized, style: UIAlertActionStyle.default, handler: {
+                (action: UIAlertAction!) in
+                let format:Format? = self.formats?[indexPath.row]
+                let context = format?.managedObjectContext
+                context?.delete(format!)
+                self.formats?.remove(at: indexPath.row)
+                try! context?.save()
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            })
+            let cancel = UIAlertAction(title: "cancel".localized, style: UIAlertActionStyle.cancel, handler: {
+                (action: UIAlertAction!) in
+            })
+            
+            alert.addAction(delete)
+            alert.addAction(cancel)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        let editAction = UITableViewRowAction(style: .normal, title: "edit".localized) { (rowAction, indexPath) in
+            self.selectedFormat = self.formats![indexPath.row]
+            self.isEditMode = true
+            self.performSegue(withIdentifier: "DisplayMakeFormatView", sender: self)
+        }
+        editAction.backgroundColor = .gray
+        deleteAction.backgroundColor = .red
+        
+        return [deleteAction, editAction]
+    }
+    
+    // MARK: - IntroViewDelegate
+    
+    func close(sender: IntroView) {
+        self.view.backgroundColor = .white
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+        introView?.removeFromSuperview()
+    }
+    
+    func play(sender: IntroView) {
+        guard let path = Bundle.main.path(forResource: "tutorial", ofType:"m4v") else {
+            return
+        }
+        
+        let playerController = AVPlayerViewController()
+        playerController.player = AVPlayer(url: URL(fileURLWithPath: path))
+        self.present(playerController, animated: true, completion: {
+            playerController.player!.play()
+        })
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func didPushInfoButton(sender: Any) {
+        #if DEBUG
+            self.performSegue(withIdentifier: "DebugView", sender: self)
+        #else
+            showIntro()
+        #endif
+    }
+    
+    // MARK: -
     
     func showTipView() {
         let popTip = PopTip()
@@ -65,36 +180,9 @@ class FormatViewController: UITableViewController, IntroViewDelegate {
         self.view.addSubview(introView!)
     }
     
-    // MARK: - IntroViewDelegate
-    
-    func close(sender: IntroView) {
-        self.view.backgroundColor = .white
-        self.navigationController?.setNavigationBarHidden(false, animated: false)
-        introView?.removeFromSuperview()
-    }
-    
-    func play(sender: IntroView) {
-        guard let path = Bundle.main.path(forResource: "tutorial", ofType:"m4v") else {
-            return
-        }
-        
-        let playerController = AVPlayerViewController()
-        playerController.player = AVPlayer(url: URL(fileURLWithPath: path))
-        self.present(playerController, animated: true, completion: {
-            playerController.player!.play()
-        })
-    }
-    
+
     @objc func didPushAddButton(sender: Any) {
         self.performSegue(withIdentifier: "DisplayMakeFormatView", sender: self)
-    }
-    
-    @IBAction func didPushInfoButton(sender: Any) {
-        #if DEBUG
-            self.performSegue(withIdentifier: "DebugView", sender: self)
-        #else
-            showIntro()
-        #endif
     }
     
     func loadData() {
@@ -106,81 +194,6 @@ class FormatViewController: UITableViewController, IntroViewDelegate {
         
         formats = try! managedObjectContext!.fetch(fetchRequest)
         tableView.reloadData()
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return formats?.count ?? 0
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let format = formats![indexPath.row]
-        cell.textLabel?.text = format.name
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath)
-        cell?.setSelected(false, animated: false)
-        
-        selectedFormat = formats![indexPath.row]
-        self.performSegue(withIdentifier: "DisplayAnswerSheetListView", sender: self)
-    }
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        let deleteAction = UITableViewRowAction(style: .normal, title: "delete".localized) { (rowAction, indexPath) in
-            let alert = UIAlertController(title:"", message: "confirm delete".localized,
-                                          preferredStyle: UIAlertControllerStyle.alert)
-            let delete = UIAlertAction(title: "delete".localized, style: UIAlertActionStyle.default, handler: {
-                (action: UIAlertAction!) in
-                let format:Format? = self.formats?[indexPath.row]
-                let context = format?.managedObjectContext
-                context?.delete(format!)
-                self.formats?.remove(at: indexPath.row)
-                try! context?.save()
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            })
-            let cancel = UIAlertAction(title: "cancel".localized, style: UIAlertActionStyle.cancel, handler: {
-                (action: UIAlertAction!) in
-            })
-
-            alert.addAction(delete)
-            alert.addAction(cancel)
-
-            self.present(alert, animated: true, completion: nil)
-        }
-        
-        let editAction = UITableViewRowAction(style: .normal, title: "edit".localized) { (rowAction, indexPath) in
-            self.selectedFormat = self.formats![indexPath.row]
-            self.isEditMode = true
-            self.performSegue(withIdentifier: "DisplayMakeFormatView", sender: self)
-        }
-        editAction.backgroundColor = .gray
-        deleteAction.backgroundColor = .red
-        
-        return [deleteAction, editAction]
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "DisplayAnswerSheetListView" {
-            let destinationVC = segue.destination as! AnswerSheetListViewController
-            destinationVC.format = selectedFormat
-        } else if segue.identifier == "DisplayMakeFormatView" {
-            let nc = segue.destination as! UINavigationController
-            let destinationVC = nc.topViewController as! MakeFormatViewController
-            if isEditMode {
-                destinationVC.editFormat = selectedFormat
-                isEditMode = false
-            }
-            destinationVC.managedObjectContext = managedObjectContext!
-            destinationVC.completionHandler = {
-                self.loadData()
-            }
-        }
     }
 }
 

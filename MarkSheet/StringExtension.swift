8467e48ab4832dc59767bd095cc06a204be1fb2b
xenop <nejisiki2047@gmail.com>
2017-12-21 15:15:58 +0900

    remove debug code

--- MarkSheet/StringExtension.swift
+++ MarkSheet/StringExtension.swift
@@ -10,16 +10,6 @@ import Foundation
 
 extension String {
     var localized: String {
-        if let _ = UserDefaults.standard.string(forKey: "i18n_language") {} else {
-            let pre = Locale.preferredLanguages[0]
-            UserDefaults.standard.set(pre, forKey: "i18n_language")
-        }
-        
-        let lang = UserDefaults.standard.string(forKey: "i18n_language")
-        
-        let path = Bundle.main.path(forResource: lang, ofType: "lproj")
-        let bundle = Bundle(path: path!)
-        
-        return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
+        return NSLocalizedString(self, comment: "")
     }
 }
